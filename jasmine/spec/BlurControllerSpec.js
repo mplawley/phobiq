e5bab6675b60759c39a96fc1733734df451d6734
Mark Lawley <mark.lawley@gmail.com>
2017-12-29 12:32:10 -0500

    Test build out

--- jasmine/spec/BlurControllerSpec.js
+++ jasmine/spec/BlurControllerSpec.js
@@ -1,9 +1,21 @@
-describe("Test blur decrement", function() {
+describe("Test BlurController functionality", function() {
   var blurController;
 
   beforeEach(function() {
     blurController = new BlurController();
     blurController.initializeValues();
+    blurController.bindUserInterfaceElements();
+    blurController.initializeUserInterfaceValues();
+  });
+
+  it("should be able to initialize BlurController values", function() {
+    var expectedMaxBlur = 100;
+    var expectedCurrentBlur = 100;
+    var expectedBlurStep = 1;
+
+    expect(expectedMaxBlur).toEqual(blurController.getMaxBlur());
+    expect(expectedCurrentBlur).toEqual(blurController.getCurrentBlur());
+    expect(expectedBlurStep).toEqual(blurController.getBlurStep());
   });
 
 	it("should be able to decrement currentBlur by blurStep", function() {
@@ -16,4 +28,30 @@ describe("Test blur decrement", function() {
     var expectedValue = actualCurrentBlur - actualBlurStep;
     expect(expectedValue).toEqual(blurController.getCurrentBlur());
 	});
+
+  it("should be able to set the currentBlur directly", function() {
+    blurController.setCurrentBlur(45);
+    expect(45).toEqual(blurController.getCurrentBlur());
+  });
+
+  it("should ensure that unblur() is called", function() {
+    spyOn(blurController, "unblur");
+    blurController.unblur(); 
+    expect(blurController.unblur).toHaveBeenCalled();
+  });
+
+  //TODO: update after downloading jquery-jasmine
+  // it("should have an interactive image container", function() {
+  //   var spyOnImageEvent = spyOn(blurController, "unblur");
+  //   $("#images").trigger("click");
+  //   expect(blurController.unblur).toHaveBeenCalled();
+  //   ($("#images"));
+  //   expect(clickImagesEvent).toHaveBeenTriggered();
+  //   expect(99).toEqual(blurController.getCurrentBlur());
+  //   var eventSpy = spyOnEvent('#imageContainer', 'click');
+  //   $('#imageContainer').trigger('click');
+  //   expect('click').toHaveBeenTriggeredOn('#imageContainer');
+  //   expect(eventSpy).toHaveBeenTriggered();
+  //   pending();
+  // });
 });
