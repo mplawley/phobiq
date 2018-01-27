5aa0a2f45d7a4fe29ec07416f9aeff1d20bc8475
Mark Lawley <mark.lawley@gmail.com>
2018-01-26 20:33:52 -0500

    Error messages on failed username or password at login

--- login.php
+++ login.php
@@ -72,13 +72,6 @@
         // Close connection
         $mysqli->close();
     }
-
-    if (!empty($username_err)) {
-        echo "username_err: " . $username_err;
-    }
-    if (!empty($password_err)) {
-        echo "password_err: " .  $password_err;
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -111,12 +104,20 @@
       <input id="login-button" type="submit" name="Login" value="Login" class="login-submit"/>
 
       <a id="registerLink" href="/register.php">Not a member yet? Register for free!</a>
-    </form>
 
-    <div id="errors">
-        <ul id="errorList">
-        </ul>
-    </div>
+        <div id="errors">
+            <ul id="errorList">
+                <?php
+                    if (!empty($username_err)) {
+                        echo $username_err;
+                    }
+                    if (!empty($password_err)) {
+                        echo $password_err;
+                    }
+                ?>
+            </ul>
+        </div>
+    </form>
 
     <a href="#" class="login-forgot-pass">forgot password?</a>
     <div class="underlay-photo"></div>
