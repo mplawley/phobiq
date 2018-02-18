180204f21cd159c7f8e20a4fb319e15ae52f9093
Mark Lawley <mark.lawley@gmail.com>
2018-02-18 00:06:31 -0500

    Update comment and spec link

--- jasmine/src/Main.js
+++ jasmine/src/Main.js
@@ -2,8 +2,7 @@
  * Phobiq: This controller builds the core objects we need
  * to use Phobiq, such as the blurController. While we aim to 
  * decouple these controllers, whenever there is a dependency between
- * controllers, we inject the dependency through the constructor 
- * argument.
+ * controllers, we pass the dependency in as an argument.
  */
 
 $(document).ready(function() {
